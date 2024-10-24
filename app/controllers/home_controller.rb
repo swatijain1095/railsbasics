0cafc6320b0e52e28fbf2f8158351af4b99ff64e
Swati Jain <jain.swati71@gmail.com>
2024-10-24 14:56:10 +0200

    updated index action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,12 @@
 class HomeController < ApplicationController
-
   def index
-    #render plain: "Hello, World!"
+    # render plain: "Hello, World!"
     message="<h1>Hello, World!</h1>"
     message+="<h2>Hello, World!</h2>"
     message+="<h3>Hello, World!</h3>"
     message+="<h4>Hello, World!</h4>"
     message+="<h5>Hello, World!</h5>"
     message+="<h6>Hello, World!</h6>"
-
     render html: message.html_safe
-
   end
 end
