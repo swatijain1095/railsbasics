50dc526cb06dd92a5875e7f2978f86471de3e729
Swati Jain <jain.swati71@gmail.com>
2024-10-25 15:10:19 +0200

    added logic to access querystring parameters

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,4 +9,24 @@ class HomeController < ApplicationController
     message+="<h6>Hello, World!</h6>"
     render html: message.html_safe
   end
+
+  def getquerystringvalues
+    message=""
+    if params[:message]
+      message+="<h1>Message is: " + params[:message] + "</h1>"
+    else
+      message+="<h1>You have not supplied any value for message</h1>"
+    end
+
+  if params[:country]
+    message+="<h1>Country is: " + params[:country] + "</h1>"
+  else
+    message+="<h1>You have not supplied any value for country</h1>"
+  end
+  message+="<h1>Controller name is: "+ params[:controller] + "</h1>"
+  message+="<h1>Action name is: "+ params[:action] + "</h1>"
+
+render html: message.html_safe
+end
+
 end
