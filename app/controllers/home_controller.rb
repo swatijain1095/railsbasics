6db4a6e96b4f1b8b3be48c0ab0f2053320b8eec8
Swati Jain <jain.swati71@gmail.com>
2024-10-26 18:40:44 +0200

    debugging

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -53,9 +53,12 @@ end
 
 def loopthrougharrays
   @Products=[]
+  # debugger
   @Products.push({ "id": 1, "name": "Laptop", "price": 10000 })
+  # debugger
   @Products.push({ "id": 2, "name": "Mobile", "price": 20000 })
   @Products.push({ "id": 3, "name": "Tablet", "price": 30000 })
+  # debugger
   @Products.push({ "id": 4, "name": "Television", "price": 40000 })
   @Products.push({ "id": 5, "name": "Camera", "price": 50000 })
   @Products.push({ "id": 6, "name": "Watch", "price": 60000 })
