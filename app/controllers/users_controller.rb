f13f2e5dd9bc091697aecfe2646d9461ee59f05e
Swati Jain <jain.swati71@gmail.com>
2024-11-20 13:21:17 +0100

    fixed linting issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,11 @@ class UsersController < ApplicationController
     if !@user.save
       render :new, status: :unprocessable_entity
     end
-  end
-
-  # Brakeman: ignore Mass Assignment warning for 'role' and 'notes'.
+  end  
   private
   def user_params
+  # Brakeman: ignore Mass Assignment warning for 'role' and 'notes'.
+  # Role is restricted to admin users, and Notes is validated in the model.
     params.require(:user).permit(:name, :password, :password_confirmation, :gender, :email, :birthdate, :phone, :postalcode, :websiteurl, :termsandcondition, :role, :notes)
   end
 end
