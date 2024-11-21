470c44436077bdd587b600a55b7a12d7c71a5f1f
Swati Jain <jain.swati71@gmail.com>
2024-11-21 21:23:57 +0100

    added tiny_tds and activerecord-sqlserver-adapter gems and also fixed linting issue

--- Gemfile
+++ Gemfile
@@ -6,6 +6,10 @@ gem "rails", "~> 7.2.1"
 gem "propshaft"
 # Use sqlite3 as the database for Active Record
 gem "sqlite3", ">= 2.1"
+
+gem "tiny_tds"
+gem "activerecord-sqlserver-adapter"
+
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
 # Use JavaScript with ESM import maps [https://github.com/rails/importmap-rails]
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def display_error(obj, field)
-    return '' if obj.blank? || obj.messages.keys.blank? || field.blank? || !obj.messages.keys.include?(field)
+    return "" if obj.blank? || obj.messages.keys.blank? || field.blank? || !obj.messages.keys.include?(field)
 
     "<span class=\"error\">#{obj[field].uniq.join('<br>')}</span>".html_safe
   end
