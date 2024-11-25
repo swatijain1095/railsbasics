f12f55fcb00d5d5e8b91b7c18499287280266c8e
Swati Jain <jain.swati71@gmail.com>
2024-11-25 11:38:45 +0100

    added deletion of bulk records using checkboxes

--- app/controllers/bulkdeleteemployees_controller.rb
+++ app/controllers/bulkdeleteemployees_controller.rb
@@ -1,2 +1,12 @@
 class BulkdeleteemployeesController < ApplicationController
+  def index
+    @employees = Employee.all()
+  end
+  def bulk_delete
+    if params[:employee_ids].present?
+      puts params[:employee_ids]
+      Employee.where(id: params[:employee_ids]).destroy_all
+    end
+    redirect_to bulkdeleteemployees_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -33,4 +33,7 @@ Rails.application.routes.draw do
   resources :singleemployeedelete, only: [ :index ] do
     delete "emp_delete", on: :collection
   end
+  resources :bulkdeleteemployees, only: [ :index ] do
+    delete "bulk_delete", on: :collection
+  end
 end
