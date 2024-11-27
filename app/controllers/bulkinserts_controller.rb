49bc01ba28dd69adadab52d5425c2f1107ef6798
Swati Jain <jain.swati71@gmail.com>
2024-11-27 17:16:51 +0100

    added functionality of bulk inserts

--- app/controllers/bulkinserts_controller.rb
+++ app/controllers/bulkinserts_controller.rb
@@ -2,5 +2,14 @@ class BulkinsertsController < ApplicationController
   def new
   end
   def create
+    employees_params.each do |employee_params|
+      employee = Employee.create(employee_params)
+    end
+    redirect_to new_bulkinsert_path
+  end
+  def employees_params
+    params.require(:employees).map do |employee_params|
+      employee_params.permit(:firstname, :lastname, :phone, :email, :department_id, :country_id)
+    end
   end
 end
