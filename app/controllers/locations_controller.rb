0f7035f3736b312d7740dac65113b4f02dde0e6b
Swati Jain <jain.swati71@gmail.com>
2024-11-28 10:10:05 +0100

    added actions in locations controller which will be called using AJAX

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,10 @@
 class LocationsController < ApplicationController
+  def states
+    @states = Country.find(params[:country_id]).states
+    render json: @states
+  end
+  def cities
+    @cities = State.find(params[:state_id]).cities
+    render json: @cities
+  end
 end
