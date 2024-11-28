7b5fe73ab7da5f61cc9c2b82c9ec71880e49909e
Swati Jain <jain.swati71@gmail.com>
2024-11-28 11:49:52 +0100

    added function in cascadingselect_controller

--- app/javascript/controllers/cascadingselect_controller.js
+++ app/javascript/controllers/cascadingselect_controller.js
@@ -6,5 +6,41 @@ export default class extends Controller {
 
   static targets = ["countrySelect", "stateSelect", "citySelect"];
 
-  loadStates() {}
+  loadStates() {
+    const countryId = this.countrySelectTarget.value;
+    if (countryId) {
+      Rails.ajax({
+        type: "GET",
+        url: `/locations/states?country_id=${country_id}`,
+        success: (data) => {
+          if (data.length > 0) {
+            this.updateOptions(this.stateSelectTarget, data, "");
+          } else {
+            this.updateOptions(this.stateSelectTarget, data, "Select State");
+            this.updateOptions(this.citySelectTarget, [], "Select City");
+          }
+        },
+      });
+    } else {
+      this.stateSelectTarget.innerHTML = "Select State";
+      this.citySelectTarget.innerHTML = "Select City";
+    }
+  }
+
+  updateOptions(container, data, prompt) {
+    container.innerHTML = this.generateOptions(data, prompt);
+  }
+
+  generateOptions(data, prompt) {
+    const options = data.map(
+      (option) => `<option value="${option.id}">${option.name}</option>`
+    );
+    if (prompt) {
+      return `<option value="">${prompt}</option>${options.join("")}`;
+    } else {
+      return options;
+    }
+  }
+
+  loadCities() {}
 }
