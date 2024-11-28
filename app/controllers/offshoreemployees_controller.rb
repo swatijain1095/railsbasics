ec87ea2e3a052ae7162fd6b4be8ac1106ac98b91
Swati Jain <jain.swati71@gmail.com>
2024-11-28 10:07:31 +0100

    added new and create action in offshoreemployee controller

--- app/controllers/offshoreemployees_controller.rb
+++ app/controllers/offshoreemployees_controller.rb
@@ -1,2 +1,16 @@
 class OffshoreemployeesController < ApplicationController
+  def new
+    @offshoreemployee = offshoreemployee.new
+  end
+  def create
+    @offshoreemployee = offshoreemployee.new(offshoreemployee_params)
+    if @offshoreemployee.save
+      redirect_to new_offshoreemployee_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  def offshoreemployee_params
+    params.require(:offshoreemployee).permit(:firstname, :lastname, :country_id, :state_id, :city_id)
+  end
 end
