4a678012ebaf979762ce523d737a93e8676e8b93
Swati Jain <jain.swati71@gmail.com>
2024-12-03 13:55:47 +0100

    fixed linting issue

--- config/routes.rb
+++ config/routes.rb
@@ -49,5 +49,4 @@ Rails.application.routes.draw do
   get "/shoppingcart/product_list" => "shoppingcart#product_list"
   get "/shoppingcart/:product_id/add_cart" => "shoppingcart#add_cart", as: :add_cart
   get "/shoppingcart/:product_id/remove_cart" => "shoppingcart#remove_cart", as: :remove_cart
-
 end
