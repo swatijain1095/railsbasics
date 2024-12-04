837afbc7679e4596dbde2b611e75cb967d0fd5ed
Swati Jain <jain.swati71@gmail.com>
2024-12-04 20:35:23 +0100

    saving employee allowances in multiple data upon submission of form

--- app/controllers/allowances_controller.rb
+++ app/controllers/allowances_controller.rb
@@ -5,5 +5,18 @@ class AllowancesController < ApplicationController
   end
 
   def radio_create
+    @employee_salary = Employeesalary.new(employee_salary_params)
+    if @employee_salary.save
+      @employee_salary.employeeallowances.create(allowancetype_id: params[:allowancetype_id])
+      redirect_to radio_index_path, notice: "Employee salary is successfully created."
+    else
+      render :radio_index
+    end
+  end
+
+  private
+
+  def employee_salary_params
+    params.require(:employeesalary).permit(:salary, :netallowanceamount, :netsalary)
   end
 end
