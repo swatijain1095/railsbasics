7acfa4e2aa00a4ee42eae93175228215213e3705
Swati Jain <jain.swati71@gmail.com>
2024-12-04 10:03:06 +0100

    implemented dynamic page sizing by introducing dropdown

--- app/controllers/employeespaginations_controller.rb
+++ app/controllers/employeespaginations_controller.rb
@@ -3,6 +3,7 @@ class EmployeespaginationsController < ApplicationController
     #Pagination
     # @employees = Employee.all
     # @employees = Employee.page(params[:page])
-    @employees = Employee.page(params[:page]).per(2)
+    # @employees = Employee.page(params[:page]).per(2)
+    @employees = Employee.page(params[:page]).per(params[:per_page])
   end
 end
