44809f4c10761a0a6f069b42590a2a4ada9eebde
Swati Jain <jain.swati71@gmail.com>
2024-12-04 20:14:57 +0100

    added calculate function

--- app/javascript/controllers/salary_controller.js
+++ app/javascript/controllers/salary_controller.js
@@ -6,5 +6,26 @@ export default class extends Controller {
 
   static targets = ["salaryInput", "allowanceSalary", "netSalary"];
 
-  calculate() {}
+  calculate() {
+    const salary = parseFloat(this.salaryInputTarget.value);
+    const selectedRadioButton = document.querySelector(
+      'input[name="allowancetype_id"]:checked'
+    );
+    if (!selectedRadioButton) {
+      alert("Please Select atleast one allowance type!");
+      return;
+    }
+
+    const allowancePercentage = parseFloat(
+      selectedRadioButton.dataset.percentage
+    );
+    if (isNaN(salary) || isNaN(allowancePercentage)) {
+      return;
+    }
+
+    const allowanceAmount = (salary * allowancePercentage) / 100;
+    this.allowanceSalaryTarget.value = allowanceAmount.toFixed(2);
+    const netSalary = salary + allowanceAmount;
+    this.netSalaryTarget.value = netSalary.toFixed(2);
+  }
 }
