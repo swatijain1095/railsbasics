732c5d33cbf896fa825fd71e72f796857d85f6f9
Swati Jain <jain.swati71@gmail.com>
2024-12-05 12:31:30 +0100

    rendering items in listboxes and also created view and defined targets

--- app/javascript/controllers/listbox_controller.js
+++ app/javascript/controllers/listbox_controller.js
@@ -1,7 +1,19 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="listbox"
 export default class extends Controller {
-  connect() {
-  }
+  connect() {}
+
+  static targets = [
+    "leftSelect",
+    "leftArrow",
+    "rightArrow",
+    "multipleRightArrow",
+    "multipleLeftArrow",
+    "multipleLeftArrow",
+    "rightSelect",
+    "allowanceSalary",
+    "netSalary",
+    "salaryInput",
+  ];
 }
